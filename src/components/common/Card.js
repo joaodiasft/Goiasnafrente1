f7e26ba2eebe2d410bd040ef2d909b4dbcb31d5a
JoaoClaudio <102878752+joaodiasft@users.noreply.github.com>
2025-08-14 12:27:03 -0300

    Update Card.js

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -9,7 +9,7 @@ export function createCard({ title, description, image, date, category, link }) 
         ${date ? `<small class="text-muted mb-2">${date}</small>` : ''}
         <h5 class="card-title">${title}</h5>
         <p class="card-text flex-grow-1">${description}</p>
-        <a href="${link || 'https://portal.al.go.leg.br/?fbclid=PAQ0xDSwMK3ZJleHRuA2FlbQIxMQABp6hsqpHFoh3r9k2TSymMKNc9_J_E-zrWivR_ZsF5tFOVLUYqgvG2bK0fQ_vA_aem_R3xQKs2CDXxlesqQu7W2aQ'}" class="btn btn-outline-success mt-auto">Ler mais</a>
+        <a href="${link || 'https://portal.al.go.leg.br'}" class="btn btn-outline-success mt-auto">Ler mais</a>
       </div>
     </div>
   `;
